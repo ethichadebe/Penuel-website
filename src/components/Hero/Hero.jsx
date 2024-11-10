d74888f7afe9a6891f9b38c28d749661a034f6da
ethichadebe <ethichadebe@gmail.com>
2024-11-10 11:29:13 +0200

    Scroll more accurate

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <h1>God Penuel</h1>
             <p>Speaker, author, and founder of Penuelism. Through his books, social media, and The Mamsie Foundation, he inspires and uplifts 
                 communities, sharing wisdom on finance, parenting, and life’s purpose.</p>
-            <Link to='programs' smooth={true} offset={0} duration={500}><img src={Chevron_Down} alt="" /></Link>
+            <Link to='programs' smooth={true} offset={-260} duration={500}><img src={Chevron_Down} alt="" /></Link>
         </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
         <img src={logo} alt="" className='logo' />
         <ul>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to='programs' smooth={true} offset={0} duration={500}>Shows</Link></li>
-            <li><Link to='about' smooth={true} offset={0} duration={500}>About</Link></li>
-            <li><Link to='items' smooth={true} offset={0} duration={500}>Shop</Link></li>
-            <li><Link to='icons' smooth={true} offset={0} duration={500}>Contact</Link></li>
+            <li><Link to='programs' smooth={true} offset={-260} duration={500}>Shows</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
+            <li><Link to='items' smooth={true} offset={-260} duration={500}>Shop</Link></li>
+            <li><Link to='icons' smooth={true} offset={-260} duration={500}>Contact</Link></li>
         </ul>
     </nav>
   )
