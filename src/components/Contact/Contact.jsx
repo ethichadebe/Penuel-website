08853f104a617525a19513207138fbde271cd87b
ethichadebe <ethichadebe@gmail.com>
2024-11-10 14:22:55 +0200

    Social media Icons updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,18 @@
 import React from 'react'
 import './Contact.css'
-import Envelope from "../../assets/contact/Envelope.png"
-import Facebook from "../../assets/contact/Facebook.png"
-import Instagram_Circle from "../../assets/contact/Instagram Circle.png"
-import TikTok from "../../assets/contact/TikTok.png"
-import TwitterX from "../../assets/contact/TwitterX.png"
-import YouTube from "../../assets/contact/YouTube.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faYoutube, faXTwitter, faFacebookF, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons'
 
 const Contact = () => {
   return (
     <div className='icons'>
-      <div className="icon">
-        <img src={Facebook} alt="Facebook" />
-      </div>
-      <div className="icon">
-        <img src={Envelope} alt="email" />
-      </div>
-      <div className="icon">
-        <img src={Instagram_Circle} alt="Instagram" />
-     </div>
-     <div className="icon">
-        <img src={TikTok} alt="TikTok" />
-     </div>
-     <div className="icon">
-        <img src={TwitterX} alt="TwitterX" />
-     </div>
-     <div className="icon">
-        <img src={YouTube} alt="YouTube" />
-     </div>
+      <FontAwesomeIcon icon={faYoutube} className='icon'/>
+      <FontAwesomeIcon icon={faInstagram} className='icon'/>
+      <FontAwesomeIcon icon={faFacebookF} className='icon'/>
+      <FontAwesomeIcon icon={faXTwitter} className='icon'/>
+      <FontAwesomeIcon icon={faTiktok} className='icon'/>
+      <FontAwesomeIcon icon={faEnvelope} className='icon'/>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/Logo.svg'
+import menu from '../../assets/menu.png'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
+  const [mobileMenu,setMobileMenu] = useState(false);
+  const toggleMenu = () =>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className='container'>
         <img src={logo} alt="" className='logo' />
-        <ul>
+        <ul className={mobileMenu ? '': 'hide-nav-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='programs' smooth={true} offset={-260} duration={500}>Shows</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
             <li><Link to='items' smooth={true} offset={-260} duration={500}>Shop</Link></li>
             <li><Link to='icons' smooth={true} offset={-260} duration={500}>Contact</Link></li>
         </ul>
+        <img src={menu} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
